b0d7111c7d9237092ea9afc0d7a6f1a5b24514e3
Henrique Cerquinho <henriquecerquinho@gmail.com>
2021-07-04 20:32:10 -0300

    add some comments

--- Screens/ExerciseController.gd
+++ Screens/ExerciseController.gd
@@ -18,29 +18,42 @@ var current_ex
 func _ready():
 	# generate a new random seed
 	randomize()
+	
+	# input reading scene
 	InputReader = $InputReader
+	
+	# parent node for all the notes on screen
 	NoteGroup = $VSplitContainer/MarginContainer/TextureRect/Notes
+	
+	# options panel, with many settings
 	OptionsPanel = $VSplitContainer/OptionsPanel
+	
+	# Assist node
 	Assist = $VSplitContainer/MarginContainer/TextureRect/Assist
+	Assist.visible = false
+	
 	tone_offset = $VSplitContainer/MarginContainer/TextureRect/Anchor77.position.y - $VSplitContainer/MarginContainer/TextureRect/Anchor60.position.y
 	tone_offset /= 10
+	
 	print(tone_offset)
 	_add_config_options()
 	load_exercise("RandomNote")
 
 func _process(_delta):
+	# process note hits from input
 	for note in NoteGroup.get_children():
 		for pitch in get_just_pressed_keys():
 			# hit!
 			if note.pitch == pitch:
 				print("you got it bro!")
 				current_ex.next_step()
+			# wrong note pressed :(
 			else:
 				print("man, u suck...")
 				break
 
 func load_exercise(ex_name):
-	print("loading exercise " + ex_name)
+	print(">>>loading exercise: " + ex_name)
 	var exercise_script = load("res://Exercises/" + ex_name + ".gd")
 	current_ex = exercise_script.new(self)
 
@@ -48,11 +61,8 @@ func get_note_position_by_name(note_str) -> Vector2:
 	var octave = int(note_str[0])
 	var full_note = note_str.substr(1)
 	var note = note_str[1]
-	print(octave, " - ", full_note)
 	var note_offset = NoteMapping.get_offsets()[note]
-	
 	var octave_offset = tone_offset * 7
-	
 	var dist = (note_offset * tone_offset) + ((octave - 6) * octave_offset)
 	var pos = $VSplitContainer/MarginContainer/TextureRect/Anchor60.position + Vector2(((note_offset + octave) % 2) * -50, dist)
 	return pos
@@ -67,9 +77,12 @@ func add_note(pitch):
 	var full_note = note_str.substr(1)
 	if "#" in full_note:
 		note.sharp()
+	elif "b" in full_note:
+		note.flat()
 	$VSplitContainer/MarginContainer/TextureRect/Notes.add_child(note)
 	print("add note in pitch "+str(pitch))
 
+# murders all notes on screen
 func kill_all_notes():
 	for note in NoteGroup.get_children():
 		note.queue_free()
@@ -101,6 +114,9 @@ func _on_Configs_pressed():
 	OptionsPanel.visible = not OptionsPanel.visible
 #	$OptionsPanel.popup()
 
+################################################################################
+# Options panel functions #
+
 func _add_config_options():
 	OptionsPanel.add_check_item("Assist mode", 1)
 
@@ -111,4 +127,3 @@ func _on_OptionsPanel_id_pressed(id):
 			assist_mode = not assist_mode
 			Assist.visible = assist_mode
 			OptionsPanel.set_item_checked(idx, assist_mode)
-			
