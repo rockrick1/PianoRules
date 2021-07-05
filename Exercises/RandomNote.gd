5e425f9c9eb94c9c05932322e272ba1b592a81e5
Henrique Cerquinho <henriquecerquinho@gmail.com>
2021-07-05 17:47:23 -0300

    random notes don't repeat

--- Exercises/RandomNote.gd
+++ Exercises/RandomNote.gd
@@ -2,6 +2,8 @@ extends "res://Exercises/Exercise.gd"
 
 const pitch_range = [36,96] # change this to dynamic later
 
+var last_note : int = -1
+
 func _init(controller):
 	ExerciseController = controller
 	next_step()
@@ -12,7 +14,9 @@ func _delete_current_notes():
 	ExerciseController.kill_all_notes()
 
 func _spawn_random_note():
-	var pitch = randi() % (pitch_range[1] - pitch_range[0]) + pitch_range[0]
+	var pitch = last_note
+	while pitch == last_note:
+		pitch = randi() % (pitch_range[1] - pitch_range[0]) + pitch_range[0]
 	ExerciseController.add_note(pitch)
 
 func next_step():
