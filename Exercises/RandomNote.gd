131bfee9db4cebc81131440668b9926944d29d3d
Henrique Cerquinho <henriquecerquinho@gmail.com>
2021-07-07 18:52:48 -0300

    fix random not wrking properly

--- Exercises/RandomNote.gd
+++ Exercises/RandomNote.gd
@@ -13,11 +13,13 @@ func _init(controller):
 func _delete_current_notes():
 	ExerciseController.kill_all_notes()
 
+# generates a random pitch value, different from the last
 func _spawn_random_note():
 	var pitch = last_note
 	while pitch == last_note:
 		pitch = randi() % (pitch_range[1] - pitch_range[0]) + pitch_range[0]
 	ExerciseController.add_note(pitch)
+	last_note = pitch
 
 func next_step():
 	_delete_current_notes()
